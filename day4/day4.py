44663c2796585dc93c2998c10a69dca3d9074348
Joao611 <joaofvde@gmail.com>
2019-01-21 18:52:03 +0000

    Start day 4

---
+++ day4/day4.py
@@ -0,0 +1,13 @@
+import re
+from datetime import datetime
+
+def read_input(filename):
+    f = open(filename, "r")
+    print(f.read())
+    f.close()
+
+def get_strat1(events):
+
+
+ordered_events = read_input("input.txt")
+print("Strategy 1: " . gets_strat1(ordered_events))
